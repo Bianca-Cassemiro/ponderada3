284d02891c64eec44e754d151ab7eeb6a3e95d9d
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2023-10-01 10:49:34 -0300

    ponderada4

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,31 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 from pydantic import BaseModel
 import pickle
+from model.model import User
+from model.auth.jwt_handler import signJWT
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Create the app
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Você pode especificar os métodos permitidos
+    allow_headers=["*"],  # Você pode especificar os cabeçalhos permitidos
+)
+
+users = {
+    "email":"bia@gmail.com",
+    "password":"123"
+}
 
 df = pd.read_csv('Mall_Customers.csv')
 
@@ -29,6 +48,10 @@ class InputModel(BaseModel):
 class OutputModel(BaseModel):
     prediction: float
 
+@app.options("/predict")
+async def options_predict():
+    return {"allow": "POST"}
+
 # Define predict function
 @app.post("/predict", response_model=OutputModel)
 def predict(data: InputModel):
@@ -45,5 +68,15 @@ def predict(data: InputModel):
     return {"prediction": prediction[0]}
 
 
+@app.post("/login", tags=["user"])
+def login(user: User):
+    if user.email == users["email"] and user.password == users["password"]:
+        # Autenticação bem-sucedida, retorne o token JWT ou qualquer outra resposta apropriada
+        token = signJWT(user.email)
+        return {"token": token}
+    
+    raise HTTPException(status_code=401, detail="Credenciais de login inválidas")
 if __name__ == "__main__":
     uvicorn.run(app, port=5000)
+
+
